dc4faa400eb008585a1f809ba7d59a10ba4640cd
沈成鹏 <hztqshen@gmail.com>
2018-11-10 14:32:58 +0800

    Initial commit

---
+++ src/main/java/com/ssscp/HztqApplication.java
@@ -0,0 +1,12 @@
+package com.ssscp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HztqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HztqApplication.class, args);
+	}
+}
